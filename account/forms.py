fa7a4ce72bf78e49d0737c2f3b2acc899ead5450
zahra18a <zahra.rasouli.18a@gmail.com>
2025-03-17 10:24:20 +0330

    add mobile filed to user

--- account/forms.py
+++ account/forms.py
@@ -15,7 +15,7 @@ class UserCreationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ["email"]
+        fields = ["phone"]
 
     def clean_password2(self):
         # Check that the two password entries match
@@ -44,4 +44,4 @@ class UserChangeForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ["email", "password", "is_active", "is_admin"]
+        fields = ["phone", "password", "is_active", "is_admin"]
--- account/models.py
+++ account/models.py
@@ -3,29 +3,29 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, phone, password=None):
         """
         Creates and saves a User with the given email, date of
         birth and password.
         """
-        if not email:
+        if not phone:
             raise ValueError("Users must have an email address")
 
         user = self.model(
-            email=self.normalize_email(email),
+            phone=phone,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, phone, password=None):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
         """
         user = self.create_user(
-            email,
+            phone,
             password=password,
         )
         user.is_admin = True
@@ -36,16 +36,19 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser):
     email = models.EmailField(
         verbose_name="آدرس ایمیل",
+        null=True,
+        blank=True,
         max_length=255,
         unique=True,
     )
     full_name = models.CharField(max_length=50, verbose_name='نام کامل')
+    phone = models.CharField(max_length=12, unique=True, verbose_name='شماره تلفن', default='1234')
     is_active = models.BooleanField(default=True, verbose_name='فعال')
     is_admin = models.BooleanField(default=False, verbose_name='ادمین')
 
     objects = UserManager()
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = "phone"
     REQUIRED_FIELDS = []
 
     class Meta:
@@ -53,7 +56,7 @@ class User(AbstractBaseUser):
         verbose_name_plural = 'کاربرها'
 
     def __str__(self):
-        return self.email
+        return self.phone
 
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"

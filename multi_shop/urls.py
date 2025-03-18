38db51883f25439c5f1f1d6127f17f31d9708dea
zahra18a <zahra.rasouli.18a@gmail.com>
2025-03-18 14:04:57 +0330

    create page login

--- multi_shop/urls.py
+++ multi_shop/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 from . import settings
 urlpatterns = [
        path('admin/', admin.site.urls),
-       path('', include('home.urls'))
+       path('', include('home.urls')),
+       path('account/',include('account.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
